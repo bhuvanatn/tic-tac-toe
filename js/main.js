b4cb1b3307a1f5eb7e7af1900cedb19b708ed632
Bhuvana <bhuvanatn@gmail.com>
2016-06-06 23:32:05 +1000

    refactor code/written functionsfor Win X and O

--- js/main.js
+++ js/main.js
@@ -7,26 +7,19 @@ $(document).ready(function() {
     var playerTurn = playerTurn || "x";
     $("button").on('click', function() {
         $moveCount++;
-
-
         if (($(this).text() === 'x' || $(this).text() === 'o') && ($move === true)) {
-
             swal("Take some other position");
-        } else {
+         } else {
 
-            if ($move === true){
+            if ($move === true) {
                 $(this).css({
                     "background-color": "#787878",
                     "font-size": "9vh",
-                    "color" : "white",
-                    "fontWeight" : "bold"
+                    "color": "white",
+                    "fontWeight": "bold"
                 }).text(playerTurn);
             }
-            if ($moveCount >= 9) {
-                $("#winner").text("It's a Tie");
-                $move = false;
-                return;
-            }
+
             if (playerTurn === "x") {
                 playerTurn = "o";
             } else {
@@ -41,33 +34,18 @@ $(document).ready(function() {
             $c7 = $("#seven").text();
             $c8 = $("#eight").text();
             $c9 = $("#nine").text();
-            if ($c1 === "x" && $c2 === 'x' && $c3 === 'x' ||
-                $c4 === 'x' && $c5 === 'x' && $c6 === 'x' ||
-                $c7 === "x" && $c8 === 'x' && $c9 === 'x' ||
-                $c1 === 'x' && $c4 === 'x' && $c7 === 'x' ||
-                $c2 === "x" && $c5 === 'x' && $c8 === 'x' ||
-                $c3 === 'x' && $c6 === 'x' && $c9 === 'x' ||
-                $c1 === "x" && $c5 === 'x' && $c9 === 'x' ||
-                $c3 === 'x' && $c5 === 'x' && $c7 === 'x') {
-                setInterval(blinker('X Won'),5000);
-                $move = false;
-                $moveCount = 0;
-                return;
-            }
-             if ($c1 === "o" && $c2 === 'o' && $c3 === 'o' ||
-                $c4 === 'o' && $c5 === 'o' && $c6 === 'o' ||
-                $c7 === "o" && $c8 === 'o' && $c9 === 'o' ||
-                $c1 === 'o' && $c4 === 'o' && $c7 === 'o' ||
-                $c2 === "o" && $c5 === 'o' && $c8 === 'o' ||
-                $c3 === 'o' && $c6 === 'o' && $c9 === 'o' ||
-                $c1 === "o" && $c5 === 'o' && $c9 === 'o' ||
-                $c3 === 'o' && $c5 === 'o' && $c7 === 'o') {
-                setInterval(blinker('O Won'), 5000);
-                $move = false;
-                $moveCount = 0;
-            return;
-            }
 
+            var xwin = XWin();
+            if (xwin === "X Won") {
+                setBlinker(xwin);
+            }
+            var owin = OWin();
+            if (owin === "O Won") {
+                setBlinker(owin);
+            }
+            if ((xwin !== 'X Won' || owin !== 'O Won') && $moveCount >= 9) {
+                setBlinker("It's a Tie");
+            }
         }
     });
     $(".reset").on('click', function() {
@@ -76,7 +54,6 @@ $(document).ready(function() {
 
     function reset() {
         $("#one, #two, #three, #four, #five, #six, #seven, #eight, #nine").empty();
-
         $("button").css({
             "background-color": "#2C2C2C"
         });
@@ -86,11 +63,42 @@ $(document).ready(function() {
         $("#winner").text('');
     }
 
-    function blinker(x){
+    function blinker(x) {
         $("#winner").text(x).show();
         $("#winner").fadeOut(500);
         $("#winner").fadeIn(500);
+    }
 
+    function XWin() {
+        if ($c1 === "x" && $c2 === 'x' && $c3 === 'x' ||
+            $c4 === 'x' && $c5 === 'x' && $c6 === 'x' ||
+            $c7 === "x" && $c8 === 'x' && $c9 === 'x' ||
+            $c1 === 'x' && $c4 === 'x' && $c7 === 'x' ||
+            $c2 === "x" && $c5 === 'x' && $c8 === 'x' ||
+            $c3 === 'x' && $c6 === 'x' && $c9 === 'x' ||
+            $c1 === "x" && $c5 === 'x' && $c9 === 'x' ||
+            $c3 === 'x' && $c5 === 'x' && $c7 === 'x') {
+            return 'X Won';
+        }
+    }
+
+    function OWin() {
+        if ($c1 === "o" && $c2 === 'o' && $c3 === 'o' ||
+            $c4 === 'o' && $c5 === 'o' && $c6 === 'o' ||
+            $c7 === "o" && $c8 === 'o' && $c9 === 'o' ||
+            $c1 === 'o' && $c4 === 'o' && $c7 === 'o' ||
+            $c2 === "o" && $c5 === 'o' && $c8 === 'o' ||
+            $c3 === 'o' && $c6 === 'o' && $c9 === 'o' ||
+            $c1 === "o" && $c5 === 'o' && $c9 === 'o' ||
+            $c3 === 'o' && $c5 === 'o' && $c7 === 'o') {
+            return 'O Won';
+        }
+    }
+
+    function setBlinker(x) {
+        setInterval(blinker(x), 5000);
+        $move = false;
+        $moveCount = 0;
     }
 
 }); // end of document.ready()
